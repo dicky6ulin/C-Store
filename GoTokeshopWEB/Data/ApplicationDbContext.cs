7eb822d867249d11055bba59d2c1c23b93b5fcff
Dicky Agus Setiawan <dickyagussetiawan@gmail.com>
2022-06-16 17:02:58 +0700

    [FITURE] migration database

--- GoTokeshopWEB/Data/ApplicationDbContext.cs
+++ GoTokeshopWEB/Data/ApplicationDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace GoTokeshopWEB.Data
+﻿using GoTokeshopWEB.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoTokeshopWEB.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
     }
 }
