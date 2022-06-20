e6355294b538ca3a98f5a394c686a594706f72a1
Dicky Agus Setiawan <dickyagussetiawan@gmail.com>
2022-06-20 15:16:45 +0700

    [FIX] final project Razor Page

---
+++ GoTokeshopWEB/Pages/CategoryTemp/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GoTokeshopWEB.Data;
+using GoTokeshopWEB.Model;
+
+namespace GoTokeshopWEB.Pages.CategoryTemp
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly GoTokeshopWEB.Data.ApplicationDbContext _context;
+
+        public DeleteModel(GoTokeshopWEB.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Category Category { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Category == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Category = category;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Category == null)
+            {
+                return NotFound();
+            }
+            var category = await _context.Category.FindAsync(id);
+
+            if (category != null)
+            {
+                Category = category;
+                _context.Category.Remove(Category);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ GoTokeshopWEB/Pages/CategoryTemp/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GoTokeshopWEB.Data;
+using GoTokeshopWEB.Model;
+
+namespace GoTokeshopWEB.Pages.CategoryTemp
+{
+    public class IndexModel : PageModel
+    {
+        private readonly GoTokeshopWEB.Data.ApplicationDbContext _context;
+
+        public IndexModel(GoTokeshopWEB.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Category != null)
+            {
+                Category = await _context.Category.ToListAsync();
+            }
+        }
+    }
+}
